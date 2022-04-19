364e7a8089f67d639e67b2c21b3466a1f78468d1
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:44:12 +0300

    Kalıtım

---
+++ ConsoleAppKalıtım/Hayvan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKalıtım
+{
+    public class Hayvan
+    {
+        public string isim;
+        public string cinsiyet;
+        private int yas;
+
+        public void hayvanbilgigoster()
+        {
+
+            Console.WriteLine("Hayvanın İsmi : " + isim);
+            Console.WriteLine("Hayvanın Cinsiyeti : " + cinsiyet);
+
+        }
+    }
+}
---
+++ ConsoleAppKalıtım/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKalıtım
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kedi kedi = new Kedi();
+            kedi.isim = "Tekir";
+            kedi.cinsiyet = "Erkek";
+            kedi.hayvanbilgigoster();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
